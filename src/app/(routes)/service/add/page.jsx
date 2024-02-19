c46588f90804e0cceb9402b7f8d89362079101f8
Glade3796 <151785784+Glade3796@users.noreply.github.com>
2024-02-19 09:34:18 +0000

    innitalise service routes, add nav onn homepage

---
+++ src/app/(routes)/service/add/page.jsx
@@ -0,0 +1,7 @@
+export default function AddServicePage() {
+  return (
+    <div>
+      <h1>Add Service</h1>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,17 @@
 import Image from "next/image";
 import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function Home() {
-  return (<div>
-    <UserButton afterSignOutUrl="/"/>
-  </div>);
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+      <h1>Home</h1>
+      <nav>
+        <Link href="/service/slug">Single Service</Link>
+        <Link href="/service/slug/edit">Edit Service</Link>
+        <Link href="/service/add">Add Service</Link>
+      </nav>
+    </div>
+  );
 }
