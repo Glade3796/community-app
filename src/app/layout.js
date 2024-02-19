2cfca47898a384a5d30c3407b9b86f62032d1a24
Glade3796 <151785784+Glade3796@users.noreply.github.com>
2024-02-19 10:26:17 +0000

    add home (dashboard)  route

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import Link from "next/link";
+import SiteNav from "./components/SiteNav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,9 +14,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <html lang="en">
+        <body className={inter.className}>
+          <header>
+            <SiteNav />
+          </header>
+          {children}
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
