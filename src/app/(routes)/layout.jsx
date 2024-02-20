edb476a931966be1babe88fda10dbf2196444a80
Glade3796 <151785784+Glade3796@users.noreply.github.com>
2024-02-20 15:30:02 +0000

    ref:create-account force user to make a profile
    - add layout to hold function
    - add validation check

---
+++ src/app/(routes)/layout.jsx
@@ -0,0 +1,31 @@
+//force user to make profile layer
+
+import { db } from "@/_lib/db";
+
+import CreateAccount from "@/components/CreateAccount";
+import { auth } from "@clerk/nextjs";
+import React from "react";
+
+export async function validateUser() {
+  const clerk_auth_id = auth().userId;
+  console.log(clerk_auth_id);
+  const userData = await db.query(
+    `SELECT * FROM users WHERE clerk_auth_id = $1`,
+    [clerk_auth_id]
+  );
+  console.log(userData);
+  if (userData.rowCount === 0) {
+    return { exists: false, id: null };
+  }
+  return {
+    exists: true,
+    id: userData.rows[0].id,
+    username: userData.rows[0].username,
+  };
+}
+
+export default async function layout({ children }) {
+  const user = await validateUser();
+  console.log("validate user?", user);
+  return <>{user.exists ? <>{children}</> : <CreateAccount />} </>;
+}
--- src/components/CreateAccount.jsx
+++ src/components/CreateAccount.jsx
@@ -1,17 +1,16 @@
 //create account form, consider moving to component and forcing user to create an account if no profile exists for them
 
-
 import AccountForm from "@/components/AccountForm";
 
 import { auth } from "@clerk/nextjs";
 
-export default async function CreateAccountPage() {
+export default async function CreateAccount() {
   // get the clerk user id
   const clerk_auth_id = auth().userId;
   return (
     <div>
       <h1>Create Account</h1>
-      <AccountForm clerk_auth_id={clerk_auth_id} edit={false}/>
+      <AccountForm clerk_auth_id={clerk_auth_id} edit={false} />
     </div>
   );
 }
