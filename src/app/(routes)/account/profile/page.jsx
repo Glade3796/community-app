9203c328f60848d2c36ac163d4e090f7d560b06a
fzaidi96 <f.zaidi182@gmail.com>
2024-02-22 17:17:30 +0000

    amend styling

--- src/app/(routes)/account/profile/page.jsx
+++ src/app/(routes)/account/profile/page.jsx
@@ -28,30 +28,15 @@ export default async function ProfilePage() {
             </p>
             {user.verified && <p>Verified</p>}
             {user.site_admin && <p>Site admin</p>}
-
-            <Link
-              href="/account/edit"
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full shadow-lg transition-all duration-300 transform hover:scale-110 mt-4"
-            >
-              Edit profile
-            </Link>
           </div>
-          <div>
-            <Link
-              href="/account/starred"
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full shadow-lg transition-all duration-300 transform hover:scale-110 mt-4"
-            >
-              Starred Posts
-            </Link>
-          </div>
-        </div>
-        <div className="mt-6 mb-6">
+          <div className="mt-6 mb-6">
           <Link
             href="/account/edit"
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full shadow-lg transition-all duration-300 transform hover:scale-110 mt-4"
           >
             Edit profile
           </Link>
+        </div>
         </div>
       </div>
 
